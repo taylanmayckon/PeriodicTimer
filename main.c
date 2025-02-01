7f6e58b73d19c7ac1f20946c03e9fe5bac42e5e0
Taylan <eusoutaylann@gmail.com>
2025-02-01 00:17:44 -0300

    Add: Inicialização da GPIO e mensagem periódica de 1s no loop

--- main.c
+++ main.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+// GPIO do LED RGB
+#define RGB_RED 13
+#define RGB_GREEN 11
+#define RGB_BLUE 12
 
+// Definindo a máscara para ativar a GPIO
+#define OUTPUT_MASK ((1 << RGB_BLUE) | (1 << RGB_GREEN) | (1 << RGB_RED))
 
-int main()
-{
+uint16_t atraso = 3000; // Atraso em ms
+
+int main(){
     stdio_init_all();
 
+    // Inicializando a máscara da GPIO utilizada
+    gpio_init_mask(OUTPUT_MASK);
+    // Definindo como saída
+    gpio_set_dir_out_masked(OUTPUT_MASK);
+
     while (true) {
-        printf("Hello, world!\n");
+        printf("1 segundo se passou...\n");
         sleep_ms(1000);
     }
 }
